e329c7e391e97eb27164b8699be0476e63b02b3f
christopher4RY <arkarlin486@gmail.com>
2023-05-19 12:27:49 +0630

    Updated3

--- resources/js/Pages/Documents/ViewDocument.vue
+++ resources/js/Pages/Documents/ViewDocument.vue
@@ -31,8 +31,8 @@
             </h3>
             <div class="flex items-center gap-3">
               <div class="flex items-center mr-2">
-                <p class="sent"></p>
-                <p class="text-indigo-600 font-thin text-xs">Sent</p>
+                <p class="view"></p>
+                <p class="text-yellow-600 font-thin text-xs">View</p>
               </div>
               <ul class="list-disc flex gap-7 items-center">
                 <li class="flex">
@@ -46,90 +46,26 @@
         </div>
 
         <div class="flex items-center gap-3">
-          <button
-            id="invite"
-            data-dropdown-toggle="invite-dropdown"
-            class="focus:outline-none rounded text-sm border focus:border-0 peer-focus:ring-0 px-4 py-1.5 shadow-md hover:bg-gray-100 hover:shadow-0 duration-200 text-center inline-flex items-center"
-            type="button"
-          >
+          <div class="relative">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="currentColor"
-              class="w-4 h-4 text-green-600 font-bold mr-1"
+              class="w-5 h-5"
             >
               <path
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
               />
             </svg>
-
-            <span class="text-green-600 font-bold">Invite</span>
-          </button>
-          <!-- Invite Dropdown menu -->
-          <div
-            id="invite-dropdown"
-            class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow-lg w-44 dark:bg-gray-700"
-          >
-            <ul
-              class="text-sm text-gray-700 dark:text-gray-200"
-              aria-labelledby="invite"
-            >
-              <li>
-                <a
-                  href="#"
-                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >Share via link</a
-                >
-              </li>
-            </ul>
+            <p
+              :class="[noti ? 'block' : 'hidden']"
+              class="h-2 w-2 rounded-full bg-blue-500 absolute top-[-12%] right-[9%]"
+            ></p>
           </div>
-          <!-- data-modal-target="popup-modal"
-              data-modal-toggle="popup-modal" -->
-          <button
-            @click="openEmailModal"
-            class="focus:outline-none rounded bg-green-600 text-sm focus:border-0 peer-focus:ring-0 px-5 py-1.5 shadow-md hover:shadow-0 duration-200 text-center inline-flex items-center"
-          >
-            <span class="text-white font-bold">Send</span>
-          </button>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-5 h-5"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-            />
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-5 h-5"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-            />
-          </svg>
 
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -272,7 +208,7 @@
 import Echo from "laravel-echo";
 import Pusher from "pusher-js";
 import { Link, router, Head, useForm } from "@inertiajs/vue3";
-import { onMounted, reactive, ref } from "vue";
+import { onMounted, reactive, ref, watch } from "vue";
 import { initFlowbite, Modal } from "flowbite";
 import EditAside from "@/Components/Layouts/EditAside.vue";
 import ViewToolBar from "@/Components/Documents/ViewToolBar.vue";
@@ -282,7 +218,7 @@ const documents = defineProps({
   documents: Object,
   auth: Object,
 });
-
+const noti = ref(true);
 const toggle = ref(false);
 const uploadedDocument = ref("");
 const date = ref("");
@@ -301,6 +237,10 @@ const closeToggle = () => {
 
 moment(documents.documents.updated_at).format("ll");
 
+watch(noti, (newnoti, oldnoti) => {
+  console.log(newnoti.value, oldnoti.value);
+});
+
 onMounted(() => {
   window.Pusher = Pusher;
   window.Echo = new Echo({
@@ -311,6 +251,7 @@ onMounted(() => {
   });
   window.Echo.channel("document").listen("DocumentEvent", ({ document }) => {
     document && location.reload();
+    // noti.value = !noti.value;
     console.log(document);
   });
   const mainTag = document.querySelectorAll("#main");
@@ -331,10 +272,10 @@ onMounted(() => {
 });
 </script>
   <style>
-.sent {
+.view {
   width: 10px;
   height: 10px;
-  background-color: rgb(53, 53, 255);
+  background-color: rgb(255, 243, 19);
   border-radius: 50%;
   margin-right: 5px;
 }
